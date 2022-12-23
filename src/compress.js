33dd68179b4f275a326c7c7126603c6e64d45cd1
denYOUvo <59727582+denYOUvo@users.noreply.github.com>
2022-12-23 13:31:32 +0700

    Update compress.js
    fix const var

--- src/compress.js
+++ src/compress.js
@@ -3,9 +3,9 @@ const redirect = require('./redirect')
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg'
-  const compressionQuality = quality * 0.1;
+  const compressionQuality = req.params.quality * 0.1;
   
-  quality = Math.ceil(compressionQuality);
+  req.params.quality = Math.ceil(compressionQuality);
   
   sharp(input)
     .grayscale(req.params.grayscale)
